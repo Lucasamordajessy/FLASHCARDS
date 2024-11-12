fd93fba243739b69f66f79b2740509f0a54b4184
Lucasamordajessy <164033036+Lucasamordajessy@users.noreply.github.com>
2024-11-12 09:42:13 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Quem é melhor CR7 ou Messi?',
+    'Messi'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem tem copa do mundo CR7 ou Messi?',
+    'Messi'
+)
+
+criaCartao(
+    'Futebol',
+    'O CR7 tem copa do mundo?',
+    'Não'
+)
+
+criaCartao(
+    'Futebol',
+    ' Quem tem mais bola de ouro CR7 ou Messi?',
+    'Messi'
+)
